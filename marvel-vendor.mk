9caa6d3326db869a157ce2510d32a158309009a7
Shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2026-08-30 16:09:30 +0530

    marvel: Initial proprietary vendor tree structure

---
+++ marvel-vendor.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common proprietary vendor tree if present
+$(call inherit-product-if-exists, vendor/motorola/sm7750-common/sm7750-common-vendor.mk)
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/motorola/marvel
