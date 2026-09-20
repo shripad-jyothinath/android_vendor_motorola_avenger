723dc3df9cc68645ce3436b1db3fddaf9b1cff69
Shripad <shripad@marvel.device>
2026-09-20 08:58:01 +0530

    vendor: Remove invalid sm7750-common BoardConfigVendor include
    sm7750-common is not a real vendor sub-tree for marvel.
    All platform blobs are self-contained in vendor/motorola/marvel.
    The include was a leftover copied from the roadstr tree.
    
    Signed-off-by: Shripad <shripad@marvel.device>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,5 +2,5 @@
 # Copyright (C) 2026 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
-
--include vendor/motorola/sm7750-common/BoardConfigVendor.mk
+# Note: vendor/motorola/sm7750-common does not exist for marvel;
+# all platform blobs are self-contained in vendor/motorola/marvel.
